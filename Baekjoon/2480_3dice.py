85c0e0f98c84f5bc6f060b10bdcf10d5fae1bdef
Juyoung <wdd1016@naver.com>
2022-12-22 01:30:36 +0900

    ah

--- Baekjoon/2480_3dice.py
+++ Baekjoon/2480_3dice.py
@@ -4,12 +4,12 @@ while True:
 	for i in range(1,7):
 		if lst.count(i) == 3:
 			print(10000 + i * 1000)
-			break;
+			break
 	for i in range(1,7):
 		if lst.count(i) == 2:
 			print(1000 + i * 100)
-			break;
+			break
 	for i in range(6,0):
 		if lst.count(i) == 1:
 			print(i * 100)
-			break;
+			break
