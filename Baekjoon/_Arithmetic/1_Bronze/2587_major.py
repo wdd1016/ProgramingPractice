57c6bb2c339a17d99acbb2670b97d9657d4b12eb
Juyoung <wdd1016@naver.com>
2022-12-27 22:44:16 +0900

    OK

---
+++ Baekjoon/_Arithmetic/1_Bronze/2587_major.py
@@ -0,0 +1,9 @@
+lst = []
+lst.append(int(input()))
+lst.append(int(input()))
+lst.append(int(input()))
+lst.append(int(input()))
+lst.append(int(input()))
+print(sum(lst) // 5)
+lst.sort()
+print(lst[2])
