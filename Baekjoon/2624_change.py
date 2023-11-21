0e6849da3a7525f1d5690ebc33cc06b7a20afef9
Juyoung Jeong <wdd1016@gmail.com>
2023-11-21 16:59:12 +0900

    [Move] directory

--- Baekjoon/2624_change.py
+++ Baekjoon/2624_change.py
@@ -1,4 +1,5 @@
 import sys
+
 input = sys.stdin.readline
 
 t = int(input())
@@ -7,14 +8,14 @@ p = []
 n = []
 
 for _ in range(k):
-	amount, count = map(int, input().split())
-	p.append(amount)
-	n.append(count)
+    amount, count = map(int, input().split())
+    p.append(amount)
+    n.append(count)
 
 lst = [0 for _ in range(10001)]
 
-def find_exchange_count(money, lst):
-	if lst[money] == 0:
-		for i in range(k):
-			temp = find_exchange_count(money - p[i], lst)
-			if 
+# def find_exchange_count(money, lst):
+# 	if lst[money] == 0:
+# 		for i in range(k):
+# 			temp = find_exchange_count(money - p[i], lst)
+# 			if
