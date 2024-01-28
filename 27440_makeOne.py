a83e73b431b5db76627a77e9ebd460cb7a4cc3d1
Juyoung Jeong <wdd1016@gmail.com>
2024-01-29 01:05:47 +0900

    [Baekjoon, EDIT] 27440_makeOne (BFS)

--- 27440_makeOne.py
+++ 27440_makeOne.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 
 input = sys.stdin.readline
@@ -5,22 +6,25 @@ input = sys.stdin.readline
 n = int(input())
 
 minimal = dict()
-minimal[1] = 0
+minimal[n] = 0
 
+queue = deque()
+queue.append(n)
 
-def reculsion(num: int) -> int:
-    if num in minimal:
-        return minimal[num]
+while queue:
+    num = queue.popleft()
+    if num == 1:
+        print(minimal[1])
+        break
+    if num % 3 == 0 and num // 3 not in minimal:
+        queue.append(num // 3)
+        minimal[num // 3] = minimal[num] + 1
+    if num % 2 == 0 and num // 2 not in minimal:
+        queue.append(num // 2)
+        minimal[num // 2] = minimal[num] + 1
+    if num - 1 not in minimal:
+        queue.append(num - 1)
+        minimal[num - 1] = minimal[num] + 1
 
-    if num % 2 == 0 and num % 3 == 0:
-        minimal[num] = min(reculsion(num // 3), reculsion(num // 2)) + 1
-    elif num % 3 == 0:
-        minimal[num] = min(reculsion(num // 3), reculsion(num - 1)) + 1
-    elif num % 2 == 0:
-        minimal[num] = min(reculsion(num // 2), reculsion(num - 1)) + 1
-    else:
-        minimal[num] = reculsion(num - 1) + 1
-    return minimal[num]
-
-
-print(reculsion(n))
+# 6의배수에서의 num-1 제외 상황에 대한 해석이 불가능해서 BFS로 전환
+# 1에 다가갈수록 count가 증가하는 형식과 큐의 활용으로 최소횟수에 대한 정확한 접근이 가능
