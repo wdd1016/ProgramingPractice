9701d675dab9871d9f97cfa1de79d0be42c800e8
juyojeon <juyojeon@student.42seoul.kr>
2024-07-16 23:17:15 +0900

    [Baekjoon] 11727 (DP)

---
+++ Baekjoon/Silver/Dynamic_Programming/11727_tile.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+MAGIC_NUMBER = 10007
+
+n = int(input())
+numberOfCase = [0 for _ in range(n + 1)]
+numberOfCase[1] = 1
+numberOfCase[2] = 3
+
+for i in range(3, n + 1):
+    numberOfCase[i] = (numberOfCase[i - 2] * 2 + numberOfCase[i - 1]) % MAGIC_NUMBER
+
+print(numberOfCase[n])
