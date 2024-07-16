55783a66552254a39e5e3f55469506ea0176e5c4
Juyoung Jeong <wdd1016@gmail.com>
2024-07-16 23:33:14 +0900

    [baekjoon] Fix 11727

--- Baekjoon/Silver/Dynamic_Programming/11727_tile.py
+++ Baekjoon/Silver/Dynamic_Programming/11727_tile.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 MAGIC_NUMBER = 10007
 
 n = int(input())
-numberOfCase = [0 for _ in range(n + 1)]
+numberOfCase = [0 for _ in range(1002)]
 numberOfCase[1] = 1
 numberOfCase[2] = 3
 
